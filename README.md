3067b4dc90f207eebcbc1bce17eba509da0b6abe
OneSeven17 <17renatis@gmail.com>
2019-03-03 19:58:22 +0300

    autotests fixed

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://travis-ci.org/OneSeven17/HW4lessonShopsRaiting.svg?branch=master)](https://travis-ci.org/OneSeven17/HW4lessonShopsRaiting)
+[![codecov](https://codecov.io/gh/OneSeven17/HW4lessonShopsRaiting/branch/master/graph/badge.svg)](https://codecov.io/gh/OneSeven17/HW4lessonShopsRaiting)
+# Shops raiting
--- app/test/auto_test.py
+++ app/test/auto_test.py
@@ -1,33 +1,58 @@
-from app.BestTotal import best_total
+from BestTotal import best_total
 
-def best_total():
-    result = best_total([1000, 2000, 3000, 4000, 5000, 6000, 9000])
-    assert 30000 == result
+def test_best_total():
+    result = best_total([
+    [1000, 2000, 3000, 4000, 5000, 6000, 9000],
+    [1000, 2000, 3000, 4000, 5000, 6000, 7000],
+    [1000, 2000, 3000, 4000, 5000, 6000, 9000],
 
-
-from app.middle_day_sum import middle_day_sum
-
-def middle_day_sum():
-    result = middle_day_sum([1000, 2000, 3000, 4000, 5000, 6000, 9000])
-    assert 4285.714285714285 == result
+])
+    assert [0, 30000, 2, 30000] == result
 
 
-from app.max_day_total import max_day_total
+from middle_day_sum import middle_day_sum
 
-def max_day_total():
-    result = max_day_total([1000, 2000, 3000, 4000, 5000, 6000, 9000])
-    assert 9000 == result
+def test_middle_day_sum():
+    result = middle_day_sum([
+    [1000, 2000, 3000, 4000, 5000, 6000, 9000],
+    [1000, 2000, 3000, 4000, 5000, 6000, 7000],
+    [1000, 2000, 3000, 4000, 5000, 6000, 9000],
 
-
-from app.min_day_total import min_day_total
-
-def min_day_total():
-    result = min_day_total([1000, 2000, 3000, 4000, 5000, 6000, 9000])
-    assert 1000 == result
+])
+    assert [0, 4285.714285714285, 2, 4285.714285714285] == result
 
 
-from app.top3 import top_3
+from max_day_total import max_day_total
 
-def top_3():
-    result = top_3([10000, 2000, 3000, 4000, 5000, 6000, 9000])
-    assert [6000, 9000, 10000] == result
+def test_max_day_total():
+    result = max_day_total([
+    [1000, 2000, 3000, 4000, 5000, 6000, 9000],
+    [1000, 2000, 3000, 4000, 5000, 6000, 7000],
+    [1000, 2000, 3000, 4000, 5000, 6000, 9000],
+
+])
+    assert [0, 9000, 2, 9000] == result
+
+
+from min_day_total import min_day_total
+
+def test_min_day_total():
+    result = min_day_total([
+    [1000, 2000, 3000, 4000, 5000, 6000, 9000],
+    [900, 2000, 3000, 4000, 5000, 6000, 7000],
+    [1000, 2000, 3000, 4000, 5000, 6000, 9000],
+
+])
+    assert [1, 900] == result
+
+
+from top3 import top_3
+
+def test_top_3():
+    result = top_3([
+    [10000, 2000, 3000, 4000, 5000, 6000, 9000],
+    [1000, 20000, 3000, 4000, 5000, 6000, 7000],
+    [1000, 2000, 3000, 4000, 5000, 6000, 9000],
+
+])
+    assert [0, [6000, 9000, 10000], 1, [6000, 7000, 20000], 2, [5000, 6000, 9000]] == result
